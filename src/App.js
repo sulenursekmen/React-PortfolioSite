a256c966cbfcf8e17aa882e51da4f906e5911444
sulenursekmen <sulenursekmenn@gmail.com>
2024-01-23 02:31:44 +0300

    Have been fixed img issue

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
-
-import './App.css';
-import {Route,Routes } from 'react-router-dom'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
-import Menu from './components/Menu';
-import About from './components/About';
-import Contact from './components/Contact';
 import Navbar from './components/Navbar';
 import Projects from './components/Projects';
-import Blog from './components/Blog';
- import Footer from './components/Footer'
-import Certificate from './components/Certificate';
-import Education from './components/Education';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-   <Navbar/>
-    <Routes>
-      <Route path='/'  element={<Home/>}/>
-      <Route path='/menu'  element={<Menu/>}/>
-      <Route path='/about'  element={<About/>}/>
-      <Route path='/contact'  element={<Contact/>}/>
-      <Route path='/projects' element={<Projects/>}/>
-      <Route path='/certificate' element={<Certificate/>}/>
-      <Route path='/blog' element={<Blog/>}/>
-      <Route path='/education' element={<Education/>}/>
-    </Routes>
-    <Footer/> 
+      <Navbar />
+     
+   
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/projects' element={<Projects />} />
+        </Routes>
+   
+      <Footer />
     </div>
   );
 }
+
 
 export default App;
