761fc6f7a399d6b73950b9b375072436edd5acbc
sulenursekmen <sulenursekmenn@gmail.com>
2024-01-24 01:00:09 +0300

    added circular progress

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,15 @@
 
 import React, { useState, useEffect } from "react";
 import "../styles/Home.css";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 
 const Home = () => {
   const lastParagraph =
     "Software Developer | React Developer | Front end Developer";
   const typingSpeed = 25;
   const [text, setText] = useState("");
+  const [imageLoaded, setImageLoaded] = useState(false);
 
   useEffect(() => {
     const typewriter = (textIndex) => {
@@ -22,15 +25,27 @@ const Home = () => {
     return () => clearTimeout();
   }, [lastParagraph, typingSpeed]);
 
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
+
   return (
     <>
       <div className="home-container">
+        {!imageLoaded && (
+          <Box sx={{ display: "flex" }}>
+            <CircularProgress color="warning" />
+          </Box>
+        )}
         <img
-          className="background-image"
+          className={`background-image ${
+            imageLoaded ? "loaded" : "not-loaded"
+          }`}
           src="https://raw.githubusercontent.com/sulenursekmen/React-PortfolioSite/main/src/assets/black.svg"
           alt="Background"
+          onLoad={handleImageLoad}
         />
-        <div className="container">
+        <div className={`container ${imageLoaded ? "visible" : "hidden"}`}>
           <h1 className="name">Hi, I am Şule Nur Sekmen</h1>
           <p className="text">{"<" + text + "/>"}</p>
           <p className="paragraph">Skills</p>
